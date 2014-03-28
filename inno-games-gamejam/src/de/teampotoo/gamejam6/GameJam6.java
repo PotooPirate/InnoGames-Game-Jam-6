eb37e426ea68710ce56ade19331f974a1763d3a3
J. Schulz <Horstyschulz@gmail.com>
2014-03-28 22:20:23 +0100

    WORKS!!!

--- inno-games-gamejam/src/de/teampotoo/gamejam6/GameJam6.java
+++ inno-games-gamejam/src/de/teampotoo/gamejam6/GameJam6.java
@@ -1,28 +1,90 @@
 package de.teampotoo.gamejam6;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
-import de.teampotoo.gamejam6.credits.CreditsScreen;
-import de.teampotoo.gamejam6.game.GameScreen;
 import de.teampotoo.gamejam6.helper.ResourceLoader;
 import de.teampotoo.gamejam6.mainmenu.MainScreen;
 
-public class GameJam6 extends Game {
+public class GameJam6 implements ApplicationListener {
 
-	private MainScreen mMainScreen;
-	private CreditsScreen mCredetsScreen;
-	private MainScreen mHighscoreScreen;
-	private GameScreen mGameScreen;
+	public final static int STATE_MAINMENU 	= 1;
+	public final static int STATE_GAME 		= 2;
+	public final static int STATE_CREDITS 	= 3;
+	public final static int STATE_HIGHSCORE = 4;
+	
+	private int state;
+	
+	private Stage mMainMenu;
+	private Stage mGame;
+	private Stage mCredits;
+	private Stage mHighscore;
 	
 	@Override
 	public void create() {
 		ResourceLoader.loadResources();
 		
-		this.mMainScreen = new MainScreen();
-		this.mCredetsScreen = new CreditsScreen();
-		this.mHighscoreScreen = new MainScreen();
-		this.mGameScreen = new GameScreen();
-		setScreen(mMainScreen);
+		state = STATE_MAINMENU;
+		
+		mGame = new Stage();
+		mHighscore = new Stage();
+		mCredits = new Stage();
+		
+		mMainMenu = new Stage();
+		MainScreen mainScreen = new MainScreen(this);
+		mMainMenu.addActor(mainScreen);
+		
+		Gdx.input.setInputProcessor(mMainMenu);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(Gdx.gl20.GL_COLOR_BUFFER_BIT | Gdx.gl20.GL_DEPTH_BUFFER_BIT);
+		
+		switch(state) {
+			case STATE_MAINMENU:
+				mMainMenu.act(Gdx.graphics.getDeltaTime());
+				mMainMenu.draw();
+				break;
+			case STATE_CREDITS:
+				mCredits.act(Gdx.graphics.getDeltaTime());
+				mCredits.draw();
+				break;
+			case STATE_HIGHSCORE:
+				mHighscore.act(Gdx.graphics.getDeltaTime());
+				mHighscore.draw();
+				break;
+			case STATE_GAME:
+				mGame.act(Gdx.graphics.getDeltaTime());
+				mGame.draw();
+				break;
+		}
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
 	}
 	
+	public void startGame() {
+		state = STATE_GAME;
+		Gdx.input.setInputProcessor(mGame);
+	}
 }
--- inno-games-gamejam/src/de/teampotoo/gamejam6/mainmenu/MainScreen.java
+++ inno-games-gamejam/src/de/teampotoo/gamejam6/mainmenu/MainScreen.java
@@ -3,6 +3,7 @@ package de.teampotoo.gamejam6.mainmenu;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -11,97 +12,18 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
+import de.teampotoo.gamejam6.GameJam6;
 import de.teampotoo.gamejam6.helper.ResourceLoader;
 
-public class MainScreen implements Screen {
+public class MainScreen extends Group {
 
-	private Stage mStage;
+	private GameJam6 mGameJam6;
 	
-	public MainScreen() {
-		mStage = new Stage();
-		Gdx.input.setInputProcessor(mStage);
-		
-		Image startButton = new Image(ResourceLoader.BUTTON);
-		startButton.setPosition(Gdx.graphics.getWidth()/2 - startButton.getWidth()/2, Gdx.graphics.getHeight()/2 + startButton.getHeight()*2);
-		Image creditsButton = new Image(ResourceLoader.BUTTON);
-		creditsButton.setPosition(Gdx.graphics.getWidth()/2 - startButton.getWidth()/2, Gdx.graphics.getHeight()/2);
-		Image highscoreButton = new Image(ResourceLoader.BUTTON);
-		highscoreButton.setPosition(Gdx.graphics.getWidth()/2 - startButton.getWidth()/2, Gdx.graphics.getHeight()/2 - startButton.getHeight()*2);
-		
-		startButton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {
-				System.out.println("START");
-				return super.touchDown(event, x, y, pointer, button);
-			}
-		});
-		
-		creditsButton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {
-				System.out.println("CREDITS");
-				return super.touchDown(event, x, y, pointer, button);
-			}
-		});
-		
-		highscoreButton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {
-				System.out.println("HIGHSCORE");
-				return super.touchDown(event, x, y, pointer, button);
-			}
-		});
-		
-		mStage.addActor(startButton);
-		mStage.addActor(creditsButton);
-		mStage.addActor(highscoreButton);
+	public MainScreen(GameJam6 gameJam6) {
+		this.mGameJam6 = gameJam6;
+		createMenu();
 	}
 	
-	@Override
-	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(Gdx.gl20.GL_COLOR_BUFFER_BIT | Gdx.gl20.GL_DEPTH_BUFFER_BIT);
-		
-		mStage.act(delta);
-		mStage.draw();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-		mStage.getViewport().update(width, height);
-	}
-
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void dispose() {
-		mStage.dispose();
-	}
 	
 	private void createMenu() {		
 		Table table = new Table();
@@ -110,7 +32,7 @@ public class MainScreen implements Screen {
 		startButton.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
-				
+				mGameJam6.startGame();
 			}
 		});
 
@@ -134,8 +56,8 @@ public class MainScreen implements Screen {
 		table.add(creditsButton).row();
 		table.add(endButton);
 
-		table.setPosition(1280/2, 720/1.5f);
-		mStage.addActor(table);
+		table.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/1.5f);
+		this.addActor(table);
 	}
 
 }
