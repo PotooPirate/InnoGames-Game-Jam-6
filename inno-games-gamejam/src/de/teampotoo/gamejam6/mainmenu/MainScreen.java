671de5185769d2b61a4ea7aabb35643564793acc
Thoiz <tobias.hassenkloever@googlemail.com>
2014-03-29 13:21:49 +0100

    refactoring

--- inno-games-gamejam/src/de/teampotoo/gamejam6/mainmenu/MainScreen.java
+++ inno-games-gamejam/src/de/teampotoo/gamejam6/mainmenu/MainScreen.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
