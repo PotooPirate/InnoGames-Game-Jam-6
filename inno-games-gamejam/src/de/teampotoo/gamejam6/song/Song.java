caf8685862c32de978822f64b8926c41e7f0ab28
raimundwege <raimundwege@web.de>
2014-03-29 01:40:10 +0100

    small refactor

--- inno-games-gamejam/src/de/teampotoo/gamejam6/song/Song.java
+++ inno-games-gamejam/src/de/teampotoo/gamejam6/song/Song.java
@@ -14,18 +14,19 @@ public class Song implements ISong {
 	private final List<IStep> mSteps;
 	private final IGameScreen mGameScreen;
 	
+	private Music mSongMusic;	
+	
 	private float time = 0f;
 	private int index = 0;
-	private Music songMusic;
 	
 	private Song(IGameScreen gameScreen, List<IStep> steps, String musicPath) {
 		mGameScreen = gameScreen;
 		mSteps = steps;
-		songMusic = Gdx.audio.newMusic(Gdx.files.internal(musicPath));
+		mSongMusic = Gdx.audio.newMusic(Gdx.files.internal(musicPath));
 	}
 	
 	public static ISong newInstance(IGameScreen gameScreen, List<IStep> steps, String musicPath){
-		return new Song(gameScreen,steps,musicPath);
+		return new Song(gameScreen, steps, musicPath);
 	}
 	
 	@Override
@@ -36,14 +37,13 @@ public class Song implements ISong {
 		time = 0f;
 		index = 0;
 		
-		songMusic.play();
+		mSongMusic.play();
 		return true;
 	}
 
 	@Override
 	public void stop() {
-		System.out.println("song stopped");
-		songMusic.stop();
+		mSongMusic.stop();
 	}
 
 	@Override
@@ -56,8 +56,6 @@ public class Song implements ISong {
 			if (step.getFireTime() <= time) {
 				mGameScreen.fireStep(Step.newInstance(step, time));
 				index++;
-				if (!(index == mSteps.size()))
-					step = mSteps.get(index);
 			} else {
 				loop = false;
 			}
