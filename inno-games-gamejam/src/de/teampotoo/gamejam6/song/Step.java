69098e5aa57029150ed41941bceef9e259284474
Frederik Klauß <riki90@gmx.de>
2014-03-29 05:10:24 +0100

    neue Factory eingebaut (schwierigkeitsgrad abhängig)

--- inno-games-gamejam/src/de/teampotoo/gamejam6/song/Step.java
+++ inno-games-gamejam/src/de/teampotoo/gamejam6/song/Step.java
@@ -3,6 +3,9 @@ package de.teampotoo.gamejam6.song;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.MathUtils;
 
+import de.teampotoo.gamejam6.game.GameScreen;
+import de.teampotoo.gamejam6.game.IGameScreen;
+import de.teampotoo.gamejam6.game.IGameScreen.difficulty;
 import de.teampotoo.gamejam6.song.IStep.StepType;
 
 public class Step implements IStep {
@@ -11,10 +14,51 @@ public class Step implements IStep {
 	private final float mFireTime;
 	private final float mTargetTime;
 	
+	private static final float EASY_SUBSTRACT = 1.5f;
+	private static final float MEDIUM_SUBSTRACT = 1.0f;
+	private static final float HARD_SUBSTRACT = 0.5f;
+	private static final float REDIC_SUBSTRACT = 0.3f;
+	
+
 	private Step(StepType type, float fireTime, float targetTime) {
 		mType = type;
 		mFireTime = fireTime;
 		mTargetTime = targetTime;
+	}
+	
+	public static IStep newInstance(StepType type, float targetTime, IGameScreen.difficulty difficulty)
+	{
+		float fireTime;
+		switch(difficulty)
+		{
+		case easy:
+			fireTime = targetTime - EASY_SUBSTRACT;
+			if (fireTime < 0f)
+				fireTime = 0f;
+			break;
+		case medium:
+			fireTime = targetTime - MEDIUM_SUBSTRACT;
+			if (fireTime < 0f)
+				fireTime = 0f;
+			break;
+		case hard:
+			fireTime = targetTime - HARD_SUBSTRACT;
+			if (fireTime < 0f)
+				fireTime = 0f;
+			break;
+		case ridiculus:
+			fireTime = targetTime - REDIC_SUBSTRACT;
+			if (fireTime < 0f)
+				fireTime = 0f;
+			break;
+		default:
+			fireTime = targetTime - MEDIUM_SUBSTRACT;
+			if (fireTime < 0f)
+				fireTime = 0f;
+			break;			
+		}
+			
+	   return newInstance(type, fireTime, targetTime);
 	}
 	
 	public static IStep newInstance(StepType type, float fireTime, float targetTime) {
